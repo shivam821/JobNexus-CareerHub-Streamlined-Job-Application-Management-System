8ada329915a9da7308860b6b7332596466e30bad
shivam821 <73484687+shivam821@users.noreply.github.com>
2024-05-04 12:33:07 +0530

    Commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 import json
 
 app = Flask(__name__)
@@ -18,6 +19,20 @@ def apply(job_id):
         return render_template('apply.html', job=job)
     else:
         return "Job not found", 404
+    
+@app.route('/',methods = ['GET','POST'])
+def form_data():
+    if request.method =='POST' : 
+
+        full_name = request.form.get('full_name')
+        email_address = request.form.get('email_address')
+        phone_number = request.form.get('phone_number')
+        customFile = request.form.get('customFile')
+
+        print(full_name,email_address,phone_number,customFile)
+    
+    return render_template('apply')
+
 
 
 if __name__ == '__main__':
