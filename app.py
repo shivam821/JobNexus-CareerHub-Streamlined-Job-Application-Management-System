ce103996513ab2b2eb92bc3f4acc06f224ed8da7
shivam821 <73484687+shivam821@users.noreply.github.com>
2024-05-12 12:33:54 +0530

    Commit(12/05/2024)

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-import requests
 import json
 
 app = Flask(__name__)
@@ -12,6 +11,48 @@ def home():
     return render_template('home.html', jobs=JOBS, company_name='JobNexus')
 
 
+# @app.route('/apply/<int:job_id>')
+# def apply(job_id):
+#     job = next((job for job in JOBS if job['id'] == job_id), None)
+#     if job:
+#         return render_template('apply.html', job=job)
+#     else:
+#         return "Job not found", 404
+
+# @app.route('/apply/<int:job_id>')
+# def apply(job_id):
+#     job = next((job for job in JOBS if job['id'] == job_id), None)
+#     if job:
+#         return render_template('apply.html', job=job)
+#     else:
+#         return render_template('apply.html', job=None)
+    
+# @app.route('/',methods = ['GET','POST'])
+# def form_data():
+#     if request.method =='POST' : 
+
+#         full_name = request.form.get('full_name')
+#         email_address = request.form.get('email_address')
+#         phone_number = request.form.get('phone_number')
+#         customFile = request.form.get('customFile')
+
+#         print(full_name,email_address,phone_number,customFile)
+    
+#     return render_template('apply')
+
+# @app.route('/', methods=['GET', 'POST'])
+# def form_data():
+#     if request.method == 'POST':
+#         full_name = request.form.get('full_name')
+#         email_address = request.form.get('email_address')
+#         phone_number = request.form.get('phone_number')
+#         customFile = request.form.get('customFile')
+#         print(full_name, email_address, phone_number, customFile)
+
+#     # Pass the job variable, even if it's None
+#     job = None  # You can modify this line to retrieve the job data if needed
+#     return render_template('apply.html', job=job)
+
 @app.route('/apply/<int:job_id>')
 def apply(job_id):
     job = next((job for job in JOBS if job['id'] == job_id), None)
@@ -19,19 +60,20 @@ def apply(job_id):
         return render_template('apply.html', job=job)
     else:
         return "Job not found", 404
-    
-@app.route('/',methods = ['GET','POST'])
-def form_data():
-    if request.method =='POST' : 
 
+@app.route('/', methods=['GET', 'POST'])
+def form_data():
+    if request.method == 'POST':
         full_name = request.form.get('full_name')
         email_address = request.form.get('email_address')
         phone_number = request.form.get('phone_number')
         customFile = request.form.get('customFile')
-
-        print(full_name,email_address,phone_number,customFile)
+        print(full_name, email_address, phone_number, customFile)
     
-    return render_template('apply')
+    return render_template('apply.html', job={})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 
