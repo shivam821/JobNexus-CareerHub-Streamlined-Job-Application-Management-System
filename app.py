b4b32888235c0cd24924232ad03e08ee7dbe1a8b
shivam821 <73484687+shivam821@users.noreply.github.com>
2024-05-19 12:02:20 +0530

    Commit

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,render_template, request, flash, redirect, url_for
 import json
 
 app = Flask(__name__)
+app.secret_key = 'supersecretkey'
 
 with open('job_data.json','r') as file :
     JOBS = json.load(file)
@@ -9,7 +10,6 @@ with open('job_data.json','r') as file :
 @app.route('/')
 def home():
     return render_template('home.html', jobs=JOBS, company_name='JobNexus')
-
 
 # @app.route('/apply/<int:job_id>')
 # def apply(job_id):
@@ -19,26 +19,8 @@ def home():
 #     else:
 #         return "Job not found", 404
 
-# @app.route('/apply/<int:job_id>')
-# def apply(job_id):
-#     job = next((job for job in JOBS if job['id'] == job_id), None)
-#     if job:
-#         return render_template('apply.html', job=job)
-#     else:
-#         return render_template('apply.html', job=None)
-    
-# @app.route('/',methods = ['GET','POST'])
-# def form_data():
-#     if request.method =='POST' : 
 
-#         full_name = request.form.get('full_name')
-#         email_address = request.form.get('email_address')
-#         phone_number = request.form.get('phone_number')
-#         customFile = request.form.get('customFile')
 
-#         print(full_name,email_address,phone_number,customFile)
-    
-#     return render_template('apply')
 
 # @app.route('/', methods=['GET', 'POST'])
 # def form_data():
@@ -48,10 +30,26 @@ def home():
 #         phone_number = request.form.get('phone_number')
 #         customFile = request.form.get('customFile')
 #         print(full_name, email_address, phone_number, customFile)
+    
+#     return render_template('apply.html', job={})
 
-#     # Pass the job variable, even if it's None
-#     job = None  # You can modify this line to retrieve the job data if needed
-#     return render_template('apply.html', job=job)
+
+
+
+# @app.route('/form_data', methods=['POST'])
+# def form_data():
+#     if request.method == 'POST':
+#         full_name = request.form.get('full_name')
+#         email_address = request.form.get('email_address')
+#         phone_number = request.form.get('phone_number')
+#         customFile = request.form.get('customFile')
+#         print(f"Full Name: {full_name}, Email: {email_address}, Phone: {phone_number}, Resume: {customFile}")
+#         # flash(f"Application submitted for {full_name}!")
+#         # return redirect(url_for('home'))
+#         return "Form data printed to console"
+#     return "Form data not received"
+
+
 
 @app.route('/apply/<int:job_id>')
 def apply(job_id):
@@ -61,16 +59,17 @@ def apply(job_id):
     else:
         return "Job not found", 404
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/form_data', methods=['POST'])
 def form_data():
     if request.method == 'POST':
         full_name = request.form.get('full_name')
         email_address = request.form.get('email_address')
         phone_number = request.form.get('phone_number')
         customFile = request.form.get('customFile')
-        print(full_name, email_address, phone_number, customFile)
-    
-    return render_template('apply.html', job={})
+        print(f"Full Name: {full_name}, Email: {email_address}, Phone: {phone_number}, Resume: {customFile}")
+        return "Form data printed to console"
+    return "Form data not received"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
